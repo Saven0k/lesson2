bec422e05c5b36e464bab5373f5b83bf8eefe60b
Savenok <Romasav2017@gmail.com>
2022-09-03 17:20:12 +0300

    2

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@
 //782 -> 72
 //918 -> 98
 int a = new Random().Next(100 , 1000);
-int b = a / 10;
+int b = a % 10;
 int c = a / 100;
-int progress = a - (b * 10);
-int result = c * 10 + progress;
-Console.WriteLine($"{a} - > {result}");
+Console.WriteLine($" {a} -> {c}{b}");
