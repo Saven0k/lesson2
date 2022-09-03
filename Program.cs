371ca57f50c26f4fbaaa1b3e330ccf95bca3226f
Savenok <Romasav2017@gmail.com>
2022-09-03 17:49:56 +0300

    5

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿// 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
-Console.WriteLine("Введите первое число");
-int a = int.Parse(s :Console.ReadLine());
-Console.WriteLine("Введите второе  число");
-int b = int.Parse(s:Console.ReadLine());
-if (b%a == 0) Console.WriteLine($"Число {a} кратно числу {b}");
-else Console.WriteLine($"ЧИсло {a} не кратно {b} остаток от деления {b%a}");
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+Console.Clear();
+Console.WriteLine("Введите  число: ");
+int a = int.Parse( s :Console.ReadLine());
+if ((a%7 == 0)&(a%23==0)) Console.WriteLine($"Число {a} кратно числу 7 и 23");
+else Console.WriteLine($"Число {a} не кратно 7 и не кратно 23");
+if(a%7 == 0) Console.WriteLine($"{a} кратно 7");
+else Console.WriteLine($"{a} не кратно 7");
+if(a%23 == 0) Console.WriteLine($"{a} кратно 23");
+else Console.WriteLine($"{a}  не кратно 23");
+
+
+
+
+
