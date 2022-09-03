255d13afab015514c7268a55976f533390c249c3
Savenok <Romasav2017@gmail.com>
2022-09-03 21:02:40 +0300

    123

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 Console.Clear();
-Console.WriteLine("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
-if(number > 99 & number < 1000)
-{
-    int a = number / 10;
-    int b = a % 10;
-    Console.WriteLine($"{number} -> {b}");
-    
-}
-else Console.WriteLine("Число не трехзначное");
+Console.WriteLine("Введите число");
+string[] number = Console.ReadLine().Split(' ');
+Console.WriteLine(number)
+;
+
+// int a = int.Parse(number[2]);
+// int b = number.Length();
+// Console.WriteLine($"{number} -> {number[1]}");
+
+
+// if((a == 0) | (a == 1) | (a == 2) | (a == 3) | (a == 4) | (a == 5) | (a== 6) | (a == 7) | (a == 8) |(a == 9)) 
+// {
+//     Console.WriteLine($"{number} ->   {number[2]} ");
+// }
+// else Console.WriteLine($"{number} -> третьей цифры нет");
+
+// if(a == " ") Console.WriteLine($"{number} -> третьей цифры нет");
+// else Console.WriteLine($"{number} ->   {a} ");
