f9da32daa6f239563e60a7194c3f0d3564766a4d
Savenok <Romasav2017@gmail.com>
2022-09-03 17:52:34 +0300

    6

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@
 Console.Clear();
 Console.WriteLine("Введите  число: ");
 int a = int.Parse( s :Console.ReadLine());
-if ((a%7 == 0)&(a%23==0)) Console.WriteLine($"Число {a} кратно числу 7 и 23");
-else Console.WriteLine($"Число {a} не кратно 7 и не кратно 23");
-if(a%7 == 0) Console.WriteLine($"{a} кратно 7");
-else Console.WriteLine($"{a} не кратно 7");
-if(a%23 == 0) Console.WriteLine($"{a} кратно 23");
-else Console.WriteLine($"{a}  не кратно 23");
+if ((a%7 == 0)&(a%23==0)&(a != 0)) Console.WriteLine($"Число {a} кратно числу 7 и 23");
+else { Console.WriteLine($"Число {a} не кратно 7 и не кратно 23");
+    if((a%7 == 0)&(a != 0)) Console.WriteLine($"{a} кратно 7");
+    else Console.WriteLine($"{a} не кратно 7");
+    if((a%23 == 0)&(a != 0)) Console.WriteLine($"{a} кратно 23");
+    else Console.WriteLine($"{a}  не кратно 23");}
 
 
 
