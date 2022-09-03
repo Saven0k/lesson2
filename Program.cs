42d35f0816f60dbfd2ffa6097938e59c3de475ca
Savenok <Romasav2017@gmail.com>
2022-09-03 21:09:31 +0300

    1234566

--- Program.cs
+++ Program.cs
@@ -1,23 +1,14 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 Console.Clear();
-Console.WriteLine("Введите число");
-string[] number = Console.ReadLine().Split(' ');
-Console.WriteLine(number)
-;
-
-// int a = int.Parse(number[2]);
-// int b = number.Length();
-// Console.WriteLine($"{number} -> {number[1]}");
-
-
-// if((a == 0) | (a == 1) | (a == 2) | (a == 3) | (a == 4) | (a == 5) | (a== 6) | (a == 7) | (a == 8) |(a == 9)) 
-// {
-//     Console.WriteLine($"{number} ->   {number[2]} ");
-// }
-// else Console.WriteLine($"{number} -> третьей цифры нет");
-
-// if(a == " ") Console.WriteLine($"{number} -> третьей цифры нет");
-// else Console.WriteLine($"{number} ->   {a} ");
+Console.WriteLine("Введите цифру");
+int a = int.Parse(Console.ReadLine());
+if((a == 6) | ( a == 7)) 
+{
+    Console.WriteLine("Этот день являеться выходным");
+    if (a == 6) Console.WriteLine("И это суббота");
+    if (a == 7) Console.WriteLine("И это воскресенье");
+}
+else Console.WriteLine("Этот день не являеться выходным");
