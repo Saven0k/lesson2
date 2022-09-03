e6aaaebc4601d849a39b05b8a85257990ff78de9
Savenok <Romasav2017@gmail.com>
2022-09-03 18:12:02 +0300

    6

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
-﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-// 14 -> нет
-// 46 -> нет
-// 161 -> да
+﻿// 16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
 Console.Clear();
-Console.WriteLine("Введите  число: ");
-int a = int.Parse( s :Console.ReadLine());
-if ((a%7 == 0)&(a%23==0)&(a != 0)) Console.WriteLine($"Число {a} кратно числу 7 и 23");
-else { Console.WriteLine($"Число {a} не кратно 7 и не кратно 23");
-    if((a%7 == 0)&(a != 0)) Console.WriteLine($"{a} кратно 7");
-    else Console.WriteLine($"{a} не кратно 7");
-    if((a%23 == 0)&(a != 0)) Console.WriteLine($"{a} кратно 23");
-    else Console.WriteLine($"{a}  не кратно 23");}
-
-
-
-
-
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (a*a == b) Console.WriteLine($"Число {b} являеться квадратом числа {a}");
+else Console.WriteLine($"Число {b}  не являеться квадратом числа {a}");
