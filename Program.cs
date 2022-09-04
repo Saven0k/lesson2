7805d2f0d59e1d5948db7e46d6823bf94a7e2186
Savenok <Romasav2017@gmail.com>
2022-09-04 13:01:15 +0300

    12345

--- Program.cs
+++ Program.cs
@@ -4,20 +4,13 @@
 // 32679 -> 6
 Console.Clear();
 Console.WriteLine("Введите число");
-string[] number = Console.ReadLine().Split(' ');
-Console.WriteLine(number)
-;
-
-// int a = int.Parse(number[2]);
-// int b = number.Length();
-// Console.WriteLine($"{number} -> {number[1]}");
-
-
-// if((a == 0) | (a == 1) | (a == 2) | (a == 3) | (a == 4) | (a == 5) | (a== 6) | (a == 7) | (a == 8) |(a == 9)) 
-// {
-//     Console.WriteLine($"{number} ->   {number[2]} ");
-// }
-// else Console.WriteLine($"{number} -> третьей цифры нет");
-
-// if(a == " ") Console.WriteLine($"{number} -> третьей цифры нет");
-// else Console.WriteLine($"{number} ->   {a} ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberend = Convert.ToString(number);
+if (numberend.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + numberend[2]);
+}
+else 
+{
+  Console.WriteLine($"{number} -> третьей цифры нет");
+}
